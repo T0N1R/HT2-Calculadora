9afce75b3e721e37a87724edb64e60baea644ae2
DESKTOP-9JNVIM8\Antonio <reytono@gmail.com>
2018-02-02 11:16:06 -0600

    Calculadora

--- HDT2/src/Main.java
+++ HDT2/src/Main.java
@@ -6,15 +6,30 @@
 
 /**
  *
- * @author Antonio
+ * @author Antonio Reyes, Hector Miguel Valle, Rodrigo Samayoa
  */
+import java.io.*;
 public class Main {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         // TODO code application logic here
+        Calculadora main = new Calculadora();
+        File opera = new File("C:/Users/Antonio/Documents/NetBeansProjects/calculadora/CalculadoraHT2/src/prueba.txt");
+        FileReader leer = new FileReader(opera);
+        BufferedReader buff = new BufferedReader(leer);
+        
+        String linea;
+        while((linea = buff.readLine()) != null){
+          System.out.println(linea);
+          System.out.println("Respuesta: " + main.calcular(linea) + "\n");
+        }
+
+        
+        
+        
     }
     
 }
